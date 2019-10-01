a09844fc5478c4bf857ff73c5418963369243889
Celeo <celeodor@gmail.com>
2019-09-30 20:43:44 -0700

    Determining project type

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,68 @@
-use clap::{crate_version, load_yaml, App};
-use std::{process, str::FromStr};
-use strum_macros::{Display, EnumString};
+use clap::{crate_version, load_yaml, App, ArgMatches};
+use colored::Colorize;
+use std::{path::PathBuf, process, str::FromStr};
+use strum::IntoEnumIterator;
+use strum_macros::{Display, EnumIter, EnumString};
 
-#[derive(Debug, Display, EnumString)]
+#[derive(Debug, Display, EnumString, EnumIter, Copy, Clone)]
 enum ProjectType {
     Rust,
     Python,
     Node,
 }
 
-fn get_project_type() -> ProjectType {
-    unimplemented!()
+impl ProjectType {
+    fn path_matchers(self) -> Vec<PathBuf> {
+        match self {
+            ProjectType::Rust => vec![["Cargo.toml"].iter().collect()],
+            ProjectType::Python => vec![["requirements.txt"].iter().collect()],
+            ProjectType::Node => vec![["package.json"].iter().collect()],
+        }
+    }
+}
+
+fn debug(matches: &ArgMatches, message: &str) {
+    if matches.is_present("debug") {
+        println!("{}: {}", "DEBUG".cyan(), message);
+    }
+}
+
+fn get_project_type(matches: &ArgMatches) -> Option<ProjectType> {
+    for project_type in ProjectType::iter() {
+        for path in project_type.path_matchers() {
+            debug(
+                &matches,
+                &format!("Checking path {:?} for type {}", path, project_type),
+            );
+            if path.exists() {
+                return Some(project_type);
+            }
+        }
+    }
+    None
 }
 
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
+    debug(&matches, "Starting");
 
+    debug(&matches, "Determining project type");
     let project_type: ProjectType = match matches.value_of("type") {
         Some(val) => match ProjectType::from_str(val) {
             Ok(ty) => ty,
             Err(_) => {
-                println!("Error: unknown project type '{}'", val);
+                println!("{} unknown project type '{}'", "Error".red(), val);
                 process::exit(1);
             }
         },
-        None => get_project_type(),
+        None => match get_project_type(&matches) {
+            Some(ty) => ty,
+            None => {
+                println!("{} could not determine project version", "Error".red());
+                return;
+            }
+        },
     };
     println!("Project type is {}", project_type);
 }
