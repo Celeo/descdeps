28effd1753fe2e6c6449bdc432bb55f54344f90f
Celeo <celeodor@gmail.com>
2019-09-30 20:20:06 -0700

    Basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use clap::{crate_version, load_yaml, App};
+use std::{process, str::FromStr};
+use strum_macros::{Display, EnumString};
+
+#[derive(Debug, Display, EnumString)]
+enum ProjectType {
+    Rust,
+    Python,
+    Node,
+}
+
+fn get_project_type() -> ProjectType {
+    unimplemented!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
+
+    let project_type: ProjectType = match matches.value_of("type") {
+        Some(val) => match ProjectType::from_str(val) {
+            Ok(ty) => ty,
+            Err(_) => {
+                println!("Error: unknown project type '{}'", val);
+                process::exit(1);
+            }
+        },
+        None => get_project_type(),
+    };
+    println!("Project type is {}", project_type);
 }
